9df4ffb52b637acdda4994994d3508818a944e7a
aref-hammaslak <arefhammaslak>
2024-03-19 16:07:22 +0330

    Implementing Signup page and making it responsive

--- client/public/pizzaImages/index.ts
+++ client/public/pizzaImages/index.ts
@@ -2,5 +2,13 @@ import image1  from './food-3309418_1280.jpg';
 
 import image2  from './pizza-1344720_1920.jpg';
 import image3 from './pizza-2000595_1280.jpg';
+import signupImage from './4464061.jpg'
 
-export const  carasuleImages:string[] =  [image1, image2, image3];
+
+
+ const  carasuleImages:string[] =  [image1, image2, image3];
+
+export{
+  signupImage,
+  carasuleImages
+}
