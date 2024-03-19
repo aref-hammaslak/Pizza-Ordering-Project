9877bf70135d4c098498bf13b242f22c0e817fd3
aref-hammaslak <arefhammaslak>
2024-03-19 15:18:22 +0330

    Refactoring routing for Login and Signup pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import * as pages from './pages';
+import * as pages from "./pages";
 
 // React Router
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
+    path: "/menu",
+    element: <pages.Menu />,
+  },
+  {
+    path: "/signup",
+    element: <pages.Signup />,
+  },
+  {
     path: "/",
-    element: <pages.Route/>,
+    element: <pages.Route />,
     children: [
       {
         path: "/",
@@ -22,30 +27,18 @@ const router = createBrowserRouter([
         element: <pages.About />,
       },
       {
-        path: '/contact',
-        element: <pages.Contact/>
+        path: "/contact",
+        element: <pages.Contact />,
       },
-      {
-        path: "/login",
-        element: <pages.Login/>
-      },
-      {
-        path: "/signup",
-        element: <pages.Signup/>
-      },
-      {
-        path: "/menu",
-        element: <pages.Menu/>
-      },
-  
-
-
     ],
+  },
+  {
+    path: "/login",
+    element: <pages.Login />,
   },
 ]);
 
 function App() {
-  
   return (
     <>
       <QueryClientProvider client={queryClient}>
