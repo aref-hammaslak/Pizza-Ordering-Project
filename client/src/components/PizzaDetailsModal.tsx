c56dd361e1b00726a0dab50897b27aeb5e692698
aref-hammaslak <arefhammaslak>
2024-03-26 20:06:11 +0330

    Adding functionality for seeing real time price changes

--- client/src/components/PizzaDetailsModal.tsx
+++ client/src/components/PizzaDetailsModal.tsx
@@ -4,10 +4,15 @@ import { useState } from "react";
 import ModalBackground from "./ModalBackground";
 import { useNavigate } from "react-router-dom";
 import AdjustOrderQuantity from "./AdjustOrderQuantity";
-import { API_URL, useOrderExtras, usePizzas, usePostOrder } from "../hooks/useRequests";
+import {
+  API_URL,
+  useOrderExtras,
+  usePizzas,
+  usePostOrder,
+} from "../hooks/useRequests";
 import SelectionDorpDown from "./SelectionDorpDown";
-import ExtraToppingsSelector from './ExtraToppingsSelector';
-import { QueryKey, useQuery } from '@tanstack/react-query';
+import ExtraToppingsSelector from "./ExtraToppingsSelector";
+import { QueryKey, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { ovrallStatContext, OverallState } from "../App";
 import BlureImage from "./BlureImage";
@@ -49,16 +54,14 @@ type Crust = {
 };
 
 const PizzaDetailsModal = (props: pizzaDetailsModalType) => {
+  const navigate = useNavigate();
 
-   const navigate = useNavigate();
-   
-
-  //get selected pizza based on the ID 
-  const pizza = usePizzas().Pizzas.data.filter(
+  //get selected pizza based on the ID
+  const pizza:PizzaPreviwType = usePizzas().Pizzas.data.filter(
     (pizza: { id: number }) => +pizza.id == props.pizzaId
   )[0];
 
- const { isSuccess: isSucPizzas, isLoading: isLodPizzas } = usePizzas().Pizzas;
+  const { isSuccess: isSucPizzas, isLoading: isLodPizzas } = usePizzas().Pizzas;
 
   const [orderExtras, setOrderExtras] = useState<{
     toppings: Toppings[];
@@ -66,16 +69,10 @@ const PizzaDetailsModal = (props: pizzaDetailsModalType) => {
     sizes: Size[];
   }>();
 
-  
-
-
-
   const orderExtrasQuery = useOrderExtras();
   const { isSuccess: isSucOrdExt } = orderExtrasQuery;
 
-  
-  const {setOverallState, overallState} = useContext(ovrallStatContext);
- 
+  const { setOverallState, overallState } = useContext(ovrallStatContext);
 
   const [orderedPizza, setOrderedPizza] = useState<OrderedPizza>({
     notes: "",
@@ -87,8 +84,31 @@ const PizzaDetailsModal = (props: pizzaDetailsModalType) => {
     owner: 0,
   });
 
+  const [price, setPrice] = useState(pizza.cost);
 
-  
+
+  useEffect(() => {
+
+    const calculetePrice = () => {
+      const typeCost = +pizza.cost;
+      const crust = orderExtras?.crusts.filter(c => c.id === orderedPizza.crust)[0];
+      const crustPrice= crust && +crust.cost || 0;
+      const size = orderExtras?.sizes.filter(s => s.id === orderedPizza.size)[0];
+      const sizeRetio = size && +size.ratio || 1;
+      const toppings = orderExtras?.toppings.filter(t => orderedPizza.extra_topping.includes(t.id) );
+      console.log(toppings);
+      const toppinsCost = toppings && +toppings.reduce((accu , topping) => {
+        return accu + +topping.cost
+      },0) || 0;
+
+
+      const totalPrice = (typeCost + crustPrice + toppinsCost)* sizeRetio * orderedPizza.quantity;
+      setPrice(totalPrice);
+    }
+
+    calculetePrice();
+
+  }, [orderedPizza]);
 
   useEffect(() => {
     const getOrderExtras = async () => {
@@ -105,36 +125,33 @@ const PizzaDetailsModal = (props: pizzaDetailsModalType) => {
     };
 
     getOrderExtras();
-
   }, [isSucOrdExt]);
 
-  const postOrder = async (order:OrderedPizza) => {
-    order.owner = JSON.parse(localStorage.getItem('userInfo') as string).user.id;
+  const postOrder = async (order: OrderedPizza) => {
+    order.owner = JSON.parse(
+      localStorage.getItem("userInfo") as string
+    ).user.id;
     const response = await axios.post<OrderedPizza>(`${API_URL}/orders`, order);
     console.log(order);
     return response.data;
-  }
+  };
 
-  const onSubmitHandler = async (e:any) => {
-
+  const onSubmitHandler = async (e: any) => {
     e.preventDefault();
 
-    if(!localStorage.getItem('userInfo')) return navigate('/login');
+    if (!localStorage.getItem("userInfo")) return navigate("/login");
 
-    
-    const response = await  postOrder(orderedPizza);
+    const response = await postOrder(orderedPizza);
     if (response) {
       props.setModalState(false);
       setTimeout(() => {
         setOverallState({
-        ...overallState,
-        isCartOpen: true,
-      })
+          ...overallState,
+          isCartOpen: true,
+        });
       }, 300);
-      
     }
-    
-  }
+  };
 
   return (
     <ModalBackground setModalState={props.setModalState}>
@@ -143,112 +160,128 @@ const PizzaDetailsModal = (props: pizzaDetailsModalType) => {
       {isSucPizzas && (
         <div className=" absolute top-12 left-1/2 !-translate-x-1/2  ">
           <div
-          onClick={(e) => e.stopPropagation()}
-          className="bg-primary-mellow  max-w-[500px]  p-8 shadow-md rounded md:max-h-[600px] origin-center transition-transform   scrollbarmax-h-[400px] fade-forward overflow-auto"
-        >
-          <form action="" onSubmit={onSubmitHandler}>
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col  items-center gap-2">
+            onClick={(e) => e.stopPropagation()}
+            className="bg-primary-mellow  max-w-[500px]  p-8 shadow-md rounded md:max-h-[600px] origin-center transition-transform   scrollbarmax-h-[400px] fade-forward overflow-auto"
+          >
+            <form action="" onSubmit={onSubmitHandler}>
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col  items-center gap-2">
                   <div className="h-[300px] rounded overflow-hidden w-full relative ">
                     <BlureImage blureHash={pizza.blur_hash} image={pizza.image} imageAlt={pizza.name}/>
                   </div>
 
-                <div className="flex  flex-col gap-2">
-                  <h1 className="text-2xl text-center font-bold">
-                    {pizza.name}
-                  </h1>
-                  <h1 className="text-xl text-center font-bold">
-                    Price: ${pizza.cost}
-                  </h1>
+                  <div className="flex  flex-col gap-2">
+                    <h1 className="text-2xl text-center font-bold">
+                      {pizza.name}
+                    </h1>
+                    <h1 className="text-xl text-center font-bold">
+                      Price: ${(+price).toFixed(2)}
+                    </h1>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-col gap-2">
-                <h1 className="text-xl font-bold">Description:</h1>
-                <p className="text-sm">{pizza.notes}</p>
-              </div>
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-xl font-bold">Description:</h1>
+                  <p className="text-sm">{pizza.notes}</p>
+                </div>
 
-              <div className="flex justify-between ">
-                <div className="flex items-center">
-                  <label className=" font-bold items-center mr-2">Size:</label>
-                  <select
-                    className="p-2  outline-none rounded text-sm"
-                    onChange={(e) => {
+                <div className="flex justify-between ">
+                  <div className="flex items-center">
+                    <label className=" font-bold items-center mr-2">
+                      Size:
+                    </label>
+                    <select
+                      className="p-2  outline-none rounded text-sm"
+                      onChange={(e) => {
+                        setOrderedPizza({
+                          ...orderedPizza,
+                          size: +e.target.value,
+                        });
+                      }}
+                    >
+                      {orderExtras?.sizes.map((size) => (
+                        <option
+                          className="active:bg-primary-dark  rounded-none"
+                          key={size.name}
+                          value={size.id}
+                        >
+                          {size.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <AdjustOrderQuantity
+                      orderdPizza={orderedPizza}
+                      setOrderdPizz={setOrderedPizza}
+                    />
+                  </div>
+                </div>
+
+                <div className="flex justify-between ">
+                  <div className="flex items-center">
+                    <label className=" font-bold items-center mr-2">
+                      Crust:
+                    </label>
+                    <select
+                      className="p-2  outline-none rounded text-sm"
+                      onChange={(e) => {
+                        setOrderedPizza({
+                          ...orderedPizza,
+                          crust: +e.target.value,
+                        });
+                      }}
+                    >
+                      {orderExtras?.crusts.map((crust) => (
+                        <option
+                          className="active:bg-primary-dark  rounded-none"
+                          key={crust.name}
+                          value={crust.id}
+                        > 
+                          <span className="text-nowrap">
+                            {crust.name}
+                          </span>
+                          
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <ExtraToppingsSelector
+                      toppings={orderExtras?.toppings}
+                      orderedPizza={orderedPizza}
+                      setToppings={setOrderedPizza}
+                      name="Toppings"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <textarea
+                    value={orderedPizza?.notes}
+                    className="resize-none p-2 rounded focus:outline-primary-dark overflow-y-auto"
+                    onChange={(e) =>
                       setOrderedPizza({
                         ...orderedPizza,
-                        size: +e.target.value,
-                      });
-                    }}
-                  >
-                    {orderExtras?.sizes.map((size) => (
-                      <option
-                        className="active:bg-primary-dark  rounded-none"
-                        key={size.name}
-                        value={size.id}
-                      >
-                        {size.name}
-                      </option>
-                    ))}
-                  </select>
+                        notes: e.target.value,
+                      })
+                    }
+                    placeholder="Type something..."
+                    rows={3} // Specify the number of visible text lines
+                    cols={50} // Specify the number of visible text columns
+                  />
                 </div>
-                <div>
-                  <AdjustOrderQuantity orderdPizza={orderedPizza} setOrderdPizz={setOrderedPizza}  />
-                </div>
-              </div>
-              
-              <div className="flex justify-between ">
-                <div className="flex items-center">
-                  <label className=" font-bold items-center mr-2">Crust:</label>
-                  <select
-                    className="p-2  outline-none rounded text-sm"
-                    onChange={(e) => {
-                      setOrderedPizza({
-                        ...orderedPizza,
-                        crust: +e.target.value,
-                      });
-                    }}
-                  >
-                    {orderExtras?.crusts.map((crust) => (
-                      <option
-                        className="active:bg-primary-dark  rounded-none"
-                        key={crust.name}
-                        value={crust.id}
-                      >
-                        {crust.name}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div>
-                      <ExtraToppingsSelector toppings={orderExtras?.toppings} orderedPizza={orderedPizza} setToppings={setOrderedPizza}  name="Toppings" />
-                </div>
-              </div>
-            
 
-              <div>
-                <textarea
-                  value={orderedPizza?.notes}
-                  className="resize-none p-2 rounded focus:outline-primary-dark overflow-y-auto"
-                  onChange={(e) =>
-                    setOrderedPizza({ ...orderedPizza, notes: e.target.value })
-                  }
-                  placeholder="Type something..."
-                  rows={3} // Specify the number of visible text lines
-                  cols={50} // Specify the number of visible text columns
-                />
+                <button type="submit">
+                  <Button
+                    styles=" hover:text-black  hover:-translate-y-2 transition-all duration-200  lg:text-lg text-black focus:-translate-y-1  bg-primary-dark"
+                    name="Add to Cart"
+                    isLinked={false}
+                  />
+                </button>
               </div>
-
-              <button type="submit">
-                <Button
-                  styles="hover:text-primary-dark  hover:-translate-y-2 transition-all duration-200  lg:text-lg text-white focus:-translate-y-1  bg-primary-dark"
-                  name="Add to Cart"
-                  isLinked={false}
-                />
-              </button>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-        </div>
-        
       )}
     </ModalBackground>
   );
