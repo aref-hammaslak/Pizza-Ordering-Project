434bfacfc09eeb341bb293f580936221c669555a
aref-hammaslak <arefhammaslak>
2024-03-26 19:53:52 +0330

    Adding blureImage component for cart items

--- client/src/components/CartItem.tsx
+++ client/src/components/CartItem.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { CartItemType } from '../pages/Cart'
 import AdjustOrderQuantity from './AdjustOrderQuantity'
+import BlureImage from './BlureImage'
+import { QueryObserverResult, RefetchOptions } from '@tanstack/react-query'
 type CartItemProps = {
-  itemDetails: CartItemType
+  itemDetails: CartItemType,
+  refetchOrders:(options?: RefetchOptions | undefined) => Promise<QueryObserverResult<any, Error>>
 }
 const CartItem = (props:CartItemProps) => {
-  const {itemDetails } = props;
+  const {itemDetails , refetchOrders } = props;
   return (
-    <div  className='flex gap-4 border-b py-4'>
-      <div className='   '>
-        <img className='rounded-full w-24 h-24  object-cover '  src={itemDetails.img} alt="pizza imgeg" />
+    <div  className='flex last:border-none gap-4 border-b py-4'>
+      <div className='w-24 h-24 overflow-hidden  rounded-full '>
+        <BlureImage blureHash={itemDetails.blurHash} image={itemDetails.image} imageAlt={itemDetails.name} />
       </div>
       <div className='flex justify-between py-2 flex-1'>
         <div className='flex pt-2 flex-col justify-between'>
@@ -22,11 +25,11 @@ const CartItem = (props:CartItemProps) => {
         </div>
         <div className='flex flex-col justify-between items-center '>
           <div className='shadow-sm'>
-            <AdjustOrderQuantity orderId={itemDetails.id} />
+            <AdjustOrderQuantity itemDetails={itemDetails} refetchOrders={refetchOrders} orderId={itemDetails.id} quntity={itemDetails.quantity} />
           </div>
           
           <span>
-            ${itemDetails.Price.toFixed(2)}
+            ${(+itemDetails.price).toFixed(2)}
           </span>
         </div>
       </div>
