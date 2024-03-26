26121753748b273a8abf327fc4722fa85e337b71
aref-hammaslak <arefhammaslak>
2024-03-26 20:08:18 +0330

    Adding BlureImage componet for pizza preview

--- client/src/components/PizzaPreview.tsx
+++ client/src/components/PizzaPreview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PizzaPreviwType } from "../pages/Menu";
 import Button from "./Button";
+import BlureImage from "./BlureImage";
 
 type PizzaPreviewProps = {
   pizza: PizzaPreviwType;
@@ -14,14 +15,10 @@ const PizzaPreview = (props: PizzaPreviewProps) => {
     <div
       onClick={() => {props.setPizzaId(pizza.id);  props.setModalState(true)} }
       key={pizza.id}
-      className="max-w-[450px] flex flex-col  gap-4 bg-primary-mellow rounded overflow-hidden pb-4 shadow-2xl   hover:!-translate-y-1 !duration-300 transition-transform "
+      className="md:max-w-[450px] cursor-pointer sm:max-w-[375px] max-w-[330px] flex flex-col  gap-4 bg-primary-mellow rounded overflow-hidden pb-4 shadow-2xl    hover:!-translate-y-1 !duration-300 transition-transform "
     >
-      <div className="max-h-[200px] ">
-        <img
-          className=" object-cover w-full h-full "
-          src={pizza.image}
-          alt={pizza.name}
-        />
+      <div className="h-[200px] overflow-hidden w-full bg-center object-cover relative min-w-[350px]">
+       <BlureImage blureHash={pizza.blur_hash} image={pizza.image} imageAlt={pizza.name}/> 
       </div>
       <div className="flex flex-col items-center">
         <div className="flex flex-col gap-2 justify-between">
